5bdf6eeb5325472ad3e5062a669f875688a9a230
moulali39 <107177595+moulali39@users.noreply.github.com>
2022-07-30 23:47:13 +0530

    Add files via upload

---
+++ 09_even_odd.py
@@ -0,0 +1,52 @@
+# n=int(input("Enter a Number:"))
+# if n%2==0:
+#     print(f"{n} is Even Number")
+# else:
+#     print(f"{n} is Odd Number")
+
+# """ Program for one even number then rest of all odd"""
+# n=int(input("Enter a Number:"))
+# for i in range(2,n,2):
+#      print(i)
+
+# n=int(input("Enter a Number:"))
+# for b in range(1,n,2): 
+#     print(b)
+
+
+n=int(input())
+k=int(input())
+while(n<=k):
+    print("hello")
+n+=2
+
+
+# n=int(input("enter the value of n:"))
+# for i in range  (2,n,2):
+#     print(i)
+    
+# a=int(input("enter the value of a:"))
+# for i in range (17,a,2):
+#     print(i)
+    
+    
+    
+# a=int(input("enter the number a:"))
+# if  (a%2 ==0):
+#     print("this is evev number:")
+# else:
+#     print("this is odd number:")     
+
+
+
+
+# n=int(input("enter the number:"))
+# for i in range (1,n,1):
+#     print(i)
+    
+# n=int(input("enter the value of n:"))
+# k=int(input("enter the value of k:"))
+# while(n<=k):
+#     print("hello",str(n))
+# n+=1
+        
